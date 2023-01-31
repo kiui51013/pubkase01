cf432477c15b77531baf7329ea56a8469493cdd2
kiui5103 <kiui5103@gmail.com>
2023-01-31 12:31:28 +0900

    Starting here ...

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import re, os
+<<<<<<< HEAD
 from datetime import datetime
 
 print("Hello Python!")
@@ -7,3 +8,6 @@ print(datetime.now().strftime("%y年%m月%d日 %H時%M分%S秒"))
 print(os.getenv("HOME"),os.path.exists(".git"))
 ss = re.sub('\((.+?)\)', r'[\1]', '(name) (address) (telno)') # 2nd param: raw string
 print(ss)
+=======
+print("Hello Python!")
+>>>>>>> 238b2fb (Starting here ...)
