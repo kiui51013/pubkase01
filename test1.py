2ba2beb26b836431e1899fe46dc3da5bb207bec3
kiui51013 <kiui5103@gmail.com>
2023-02-02 15:19:58 +0900

    test1.py: launcher appended

--- test1.py
+++ test1.py
@@ -2,14 +2,19 @@
 import re, os
 from datetime import datetime
 
-print("Hello Python!")
-print(datetime.now().strftime("%y年%m月%d日 %H時%M分%S秒"))
-print(os.getenv("HOME"),os.path.exists(".git"))
-ss = re.sub('\((.+?)\)', r'[\1]', '(name) (address) (telno)') # 2nd param: raw string
-print(ss)
-print("Hello Python!")
+def main():
+  print("Hello Python!")
+  print(datetime.now().strftime("%y年%m月%d日 %H時%M分%S秒"))
+  print(os.getenv("HOME"),os.path.exists(".git"))
+  ss = re.sub('\((.+?)\)', r'[\1]', '(name) (address) (telno)') # 2nd param: raw string
+  print(ss)
+  print("Hello Python!")
 
-print(datetime.now().strftime("%y年%m月%d日 %H時%M分%S秒"))
-print(f"current time & date: {datetime.now()}")
+  print(datetime.now().strftime("%y年%m月%d日 %H時%M分%S秒"))
+  print(f"current time & date: {datetime.now()}")
+
 
 # this line was added at remote site. 2023/2/2(Thr)
+
+if __name__ == '__main__':
+  main()
