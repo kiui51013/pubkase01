8eb4d0cc8e47c43f849b6cfe162c0d5365512e20
kiui51013 <kiui5103@gmail.com>
2023-02-02 12:07:25 +0900

    OK2 ?

--- test1.py
+++ test1.py
@@ -8,6 +8,7 @@ print(os.getenv("HOME"),os.path.exists(".git"))
 ss = re.sub('\((.+?)\)', r'[\1]', '(name) (address) (telno)') # 2nd param: raw string
 print(ss)
 print("Hello Python!")
-print("Hello Python!")
+
 print(datetime.now().strftime("%y年%m月%d日 %H時%M分%S秒"))
+print(f"current time & date: {datetime.now()}")
 
